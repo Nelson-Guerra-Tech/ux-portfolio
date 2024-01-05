27800bdccb45c419eef38b563a06cc8397eb6d24
Nelson Guerra <nelsonguerra.tech@outlook.com>
2024-01-04 19:20:57 -0500

    add animation on scroll to contact section and d styling.

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from "@angular/core";
+import { AfterViewInit, Component, Input } from "@angular/core";
 import { ContactData } from "./data/contact.mocks";
 import { Contact } from "./models/contact.interfaces";
+import * as AOS from "aos";
+
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: "app-contact",
   templateUrl: "./contact.component.html",
   styleUrls: ["./contact.component.scss"],
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
   @Input() contact: Contact = ContactData;
+  arrow = faArrowRight;
+
+  ngAfterViewInit() {
+    AOS.init();
+  }
 }
