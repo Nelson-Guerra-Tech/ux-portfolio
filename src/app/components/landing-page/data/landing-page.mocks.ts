c2a89b27a997567c65e23e7d24a42721dff78f43
Nelson Guerra <nelsonguerra.tech@outlook.com>
2024-01-04 19:36:55 -0500

    add landing page styles and animate on scroll

--- src/app/components/landing-page/data/landing-page.mocks.ts
+++ src/app/components/landing-page/data/landing-page.mocks.ts
@@ -2,12 +2,12 @@ import { LandingPage } from "../models/landing-page.interfaces";
 
 export const LandingPageData: LandingPage = {
   label: {
-    title: "Hello! I'm Nelson",
-    subtitle: "A software engineer based in </br> Atlanta",
+    title: "Hello! I'm Nelson.",
+    subtitle: "",
   },
   descriptor: {
-    title: "Designing the Future, One Line of Code at a Time",
-    subtitle: "Software Engineer at ",
-    label: "Delta Air Lines",
+    title: "",
+    subtitle: "",
+    label: "",
   },
 };
--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { LandingPage } from "./models/landing-page.interfaces";
 import { LandingPageData } from "./data/landing-page.mocks";
+import * as AOS from "aos";
 
 @Component({
   selector: "app-landing-page",
   templateUrl: "./landing-page.component.html",
   styleUrls: ["./landing-page.component.scss"],
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
   @Input() content: LandingPage = LandingPageData;
+
+  ngOnInit() {
+    AOS.init();
+  }
 }
