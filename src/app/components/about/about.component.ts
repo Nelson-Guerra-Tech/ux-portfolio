c0917056a5895f1e56496ccbd5774d7416268dee
Nelson Guerra <nelsonguerra.tech@outlook.com>
2024-01-04 19:15:48 -0500

    add about component with styles and html strucucture

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from "@angular/core";
+import { AboutData } from "./data/about.mocks";
+import { About } from "./models/about.interfaces";
+import * as AOS from "aos";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  @Input() content: About = AboutData;
 
+  ngAfterViewInit() {
+    AOS.init();
+  }
 }
---
+++ src/app/components/about/data/about.mocks.ts
@@ -0,0 +1,12 @@
+import { About } from "../models/about.interfaces";
+
+export const AboutData: About = {
+  title:
+    "I'm currently leading design system projects and developing web applications at Delta Air Lines,",
+  subtitle:
+    "mainly focusing on customer facing applications like Delta.com and the award winning SkyMiles Program.",
+  closure:
+    "I'm enthusiastic about design systems, user experiences, and visually appealing front-end applications. As a developer I understand that not every design makes sense for users or is feasible to implement. I'm enthusiastic about finding the right balance between design and development to put users at the center of product development and designing data-driven solutions.",
+  image: "assets/images/profile-picture.png",
+  alt: "Profile Picture",
+};
